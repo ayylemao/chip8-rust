33a2c737a0b6422bcd05e5c9fa4457eec1499f54
Matthias Vogler <matthias.vogler@bluewin.ch>
2024-12-16 20:41:01 +0100

    added clock

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use sfml::graphics::RenderTarget;
 use sfml::graphics::Transformable;
 use sfml::graphics::{Color, Image, RenderWindow, Texture, Sprite};
 use sfml::window::{Event, Key, Style};
+use sfml::system::Clock;
 use crate::renderer::Renderer;
 
 // Assuming CPU is defined in another file
@@ -19,6 +20,10 @@ mod cpu; // Import the CPU module
 use crate::cpu::CPU;
 
 fn main() {
+    let display_size_x: u32 = 640;
+    let display_size_y: u32 = 320;
+
+    let mut clock = Clock::start();
 
     let mut cpu = CPU::default();
     cpu.reset();
@@ -32,8 +37,8 @@ fn main() {
         inc += 1;
     }
     
-    let mut window: RenderWindow = RenderWindow::new((640, 320),"Test", Style::CLOSE, &Default::default());
-    let mut image: Image = Image::new(640 as u32, 320 as u32);
+    let mut window: RenderWindow = RenderWindow::new((display_size_x, display_size_y),"Test", Style::CLOSE, &Default::default());
+    let mut image: Image = Image::new(display_size_x as u32, display_size_y as u32);
     let mut texture = Texture::new().unwrap();
     texture.create(64, 32);
 
@@ -41,8 +46,8 @@ fn main() {
         window: &mut window,
         image: &mut image,
         cpu: &mut cpu,
-        window_size_x: 640,
-        window_size_y: 320
+        window_size_x: display_size_x,
+        window_size_y: display_size_y
     };
     while (renderer.window.is_open()) {
         let mut event: Event;
@@ -53,14 +58,18 @@ fn main() {
             }
             
         }
-        renderer.cpu.execute();
+        renderer.window.clear(Color::BLACK);
+        if clock.elapsed_time().as_seconds() >= 0.00185 {
+            clock.restart();
+            renderer.cpu.execute();
+        }
 
         if renderer.cpu.redraw {
             renderer.draw_buffer();
             renderer.cpu.redraw = false;
         }
         let image: Image = renderer.image.clone();
-        texture.load_from_image(&image, IntRect::new(0, 0, 1000, 1000));
+        texture.load_from_image(&image, IntRect::new(0, 0, 100, 100));
         let mut sprite: Sprite = Sprite::with_texture(&texture);
         sprite.set_scale((10.0, 10.0));
         renderer.window.clear(Color::BLACK);
