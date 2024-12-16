c9856c628f04a67ae730a2351ca0a18d6a70da2e
Matthias Vogler <matthias.vogler@bluewin.ch>
2024-12-16 20:33:30 +0100

    first working version, no keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #![allow(unused)]
+use std::fs::File;
+use std::io::Read;
+use std::io::BufReader;
+
+
 mod renderer; // Import the renderer module
 
-use sfml::graphics::{RenderWindow, RenderTarget};
-use sfml::window::{Event, Style};
+use cpu::Byte;
+use sfml::graphics::IntRect;
+use sfml::graphics::RenderTarget;
+use sfml::graphics::Transformable;
+use sfml::graphics::{Color, Image, RenderWindow, Texture, Sprite};
+use sfml::window::{Event, Key, Style};
 use crate::renderer::Renderer;
 
 // Assuming CPU is defined in another file
@@ -11,4 +20,52 @@ use crate::cpu::CPU;
 
 fn main() {
 
+    let mut cpu = CPU::default();
+    cpu.reset();
+    cpu.load_sprites();
+
+    let filename = "1-chip8-logo.ch8";
+    let buffer = BufReader::new(File::open(filename).unwrap()); 
+    let mut inc: usize = 0;
+    for byte in buffer.bytes() {
+        cpu.memory[cpu.pc as usize + inc] = byte.unwrap();
+        inc += 1;
+    }
+    
+    let mut window: RenderWindow = RenderWindow::new((640, 320),"Test", Style::CLOSE, &Default::default());
+    let mut image: Image = Image::new(640 as u32, 320 as u32);
+    let mut texture = Texture::new().unwrap();
+    texture.create(64, 32);
+
+    let mut renderer: Renderer = Renderer {
+        window: &mut window,
+        image: &mut image,
+        cpu: &mut cpu,
+        window_size_x: 640,
+        window_size_y: 320
+    };
+    while (renderer.window.is_open()) {
+        let mut event: Event;
+        while let Some(event) = renderer.window.poll_event() {
+            match event {
+                Event::Closed => renderer.window.close(),
+                _ => { }
+            }
+            
+        }
+        renderer.cpu.execute();
+
+        if renderer.cpu.redraw {
+            renderer.draw_buffer();
+            renderer.cpu.redraw = false;
+        }
+        let image: Image = renderer.image.clone();
+        texture.load_from_image(&image, IntRect::new(0, 0, 1000, 1000));
+        let mut sprite: Sprite = Sprite::with_texture(&texture);
+        sprite.set_scale((10.0, 10.0));
+        renderer.window.clear(Color::BLACK);
+        renderer.window.draw(&sprite);
+        renderer.window.display();
+        
+    }
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -6,14 +6,14 @@ use crate::CPU;
 pub struct Renderer<'a> {
     pub window: &'a mut RenderWindow,
     pub cpu: &'a mut CPU,
-    pub image: Image,
+    pub image: &'a mut Image,
     pub window_size_x: u32,
     pub window_size_y: u32,
 }
 
 impl<'a> Renderer<'a> {
     pub const DISP_SIZE_X: u32 = 64;
-    pub const DISP_SIZE_Y: u32 = 64;
+    pub const DISP_SIZE_Y: u32 = 32;
     pub const HEXKEYS: [Key; 16] = [
         Key::Numpad0,
         Key::Numpad1,
@@ -33,23 +33,27 @@ impl<'a> Renderer<'a> {
         Key::F
     ];
 
-    pub fn new(window: &'a mut RenderWindow, cpu: &'a mut CPU, window_size_x: u32, window_size_y: u32) -> Self {
-        let image = Image::new(Renderer::DISP_SIZE_X, Renderer::DISP_SIZE_Y);
-        Self {window, cpu, image, window_size_x, window_size_y}
-    }
+    //pub fn new(window: &'a mut RenderWindow, cpu: &'a mut CPU, window_size_x: u32, window_size_y: u32) -> Self {
+    //    //let image = Image::new(Renderer::DISP_SIZE_X, Renderer::DISP_SIZE_Y);
+    //    Self {window, cpu, image, window_size_x, window_size_y}
+    //}
 
     pub fn draw_buffer(&mut self) {
         for y in 0..Renderer::DISP_SIZE_Y {
-            for x in 0..Renderer::DISP_SIZE_Y {
-                if self.cpu.display[y as usize][x as usize] == true {
-                    let color = if self.cpu.display[y as usize][x as usize] {
-                        Color::YELLOW
-                    } else {
-                        Color::BLACK
-                    };
-                    // TODO
+            for x in 0..Renderer::DISP_SIZE_X {
+                if self.cpu.display[y as usize][x as usize] {
+                    let color = Color::YELLOW;
+                    unsafe {
+                        self.image.set_pixel(x, y, color);
+                    }
                 }
             }
+        }
+    }
+
+    pub fn check_key_press(&mut self) {
+        for i in 0..16 {
+            self.cpu.keyboard[i] = sfml::window::Key::is_pressed(Renderer::HEXKEYS[i]);
         }
     }
 }
